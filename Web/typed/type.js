56340e72cbe6d6f612ab6a56e5e0a4d0d2e2dfea
Scramble1 <46353542+Scramble1@users.noreply.github.com>
2019-01-04 10:53:33 -0800

    added files

---
+++ Web/typed/type.js
@@ -0,0 +1,77 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I',
+      'Love',
+      'Potato',
+      'Dog',
+      'Puke'
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+
+
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'Shell Shockers',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'Olg Game shelf',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'Fruit ninja',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'Cool Game',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Another Cool game',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
